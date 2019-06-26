abaf10e1469b2dfb5aedb8e6e588912ed94dde54
Pedro Arturo Flores Ramirez <pedro@MacBook-Pro-de-Pedro.local>
2019-06-25 23:35:53 -0500

    the success commit

--- rombo.js
+++ rombo.js
@@ -2,7 +2,7 @@ const getLenght = (size) => {
   return (size * 2) - 1;
 }
 
-const getPosition = (size) => {
+const getCentralPosition = (size) => {
   return size - 1;
 };
 
@@ -14,33 +14,32 @@ const factoryArray = (lenght) => {
   return firstLine;
 };
 
-const arrayName = 'ivan'.split('');
+const arrayName = 'pedro'.split('');
 const arrayLenght = arrayName.length;
 // lenght = 7
 const totalLenght = getLenght(arrayLenght);
 // ivan - 3
-const initPosition = getPosition(arrayLenght);
-// arrayName[0] == i
-let indexName = 0;
+const centralPosition = getCentralPosition(arrayLenght);
+const results = [];
 
-let firstLine = factoryArray(totalLenght);
 
-firstLine[initPosition] = arrayName[indexName];
-
-console.log(firstLine.join(''));
-
-indexName++;
-for(indexName = indexName; indexName < arrayLenght; indexName++) {
-  let l = initPosition - 1;
-  let r = initPosition + 1
-  firstLine = factoryArray(totalLenght);
-  firstLine[initPosition] = arrayName[indexName];
+for(let indexName = 0; indexName < arrayLenght; indexName++) {
+  let l = centralPosition - 1;
+  let r = centralPosition + 1
+  let firstLine = factoryArray(totalLenght);
+  firstLine[centralPosition] = arrayName[indexName];
   for(decIndex = indexName; decIndex > 0; decIndex--) {
     firstLine[l] = firstLine[r] = arrayName[decIndex - 1];
     r++;
     l--;
   }
-  console.log(firstLine.join(''));
+  let result =  firstLine.join('');
+  console.log(result);
+  results.push(result);
+}
+
+for (let i=results.length - 2; i>=0; i--) {
+  console.log(results[i]);
 }
 
 
